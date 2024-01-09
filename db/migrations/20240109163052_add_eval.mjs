6ffe2adeed5f1f6b90a9beeb252bc83797d40f77
Viktor Kuroljov <viktor@ku.family>
2024-01-09 18:40:31 +0200

    add eval to moves

---
+++ db/migrations/20240109163052_add_eval.mjs
@@ -0,0 +1,20 @@
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = async (knex) => {
+  await knex.schema.alterTable('moves', t => {
+    t.double('eval').nullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = async (knex) => {
+  await knex.schema.alterTable('moves', t => {
+    t.dropColumn('eval')
+  })
+};
